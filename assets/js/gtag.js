b0419e3b316ac35aac36f4a20bd06182562e231e
tocapricornus <148320127+tocapricornus@users.noreply.github.com>
2023-10-19 17:53:18 +0900

    Update gtag.js

--- assets/js/gtag.js
+++ assets/js/gtag.js
@@ -1,10 +1,10 @@
 (function () {
 let gtag = document.querySelector('meta[name="analytics_gtag"]').content;
 window.dataLayer = window.dataLayer || [];
+})();
+
 function gtag(){
     dataLayer.push(arguments);
 }
 gtag('js', new Date());
 gtag('config', gtag);
-
-})();
